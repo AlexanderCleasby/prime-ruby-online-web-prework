f2b01d5370eb5f9d1fccb571842651c66f2aad3f
AlexanderCleasby <cleasby.alex@gmail.com>
2018-10-13 02:13:29 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,11 @@
 # Add  code here!
 def prime?(num)
   #if a number is divisible by 2 or 3 it is not prime:
-  if num < 0
+
+  if num % 2 == 0 && !(num==2) || num % 3 == 0 && !(num==3) || if num < 0
     return false
   end
-  if num % 2 == 0 && !(num==2)
-    return false
-  end
-  if num % 3 == 0 && !(num==3)
-    return false
-  end
+
   i=5
   w=2
   while i * i <= num
